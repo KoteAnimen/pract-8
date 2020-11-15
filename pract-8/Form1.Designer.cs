2553a1692983c5214795ed266b7e27a79c7b06c9
Kote\Kote_Animen <sverh.il@yandex.ru>
2020-11-15 15:15:59 +0300

    Исправлен размер окна

--- pract-8/Form1.Designer.cs
+++ pract-8/Form1.Designer.cs
@@ -327,7 +327,7 @@
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(264, 528);
+            this.ClientSize = new System.Drawing.Size(264, 447);
             this.Controls.Add(this.toolStrip1);
             this.Controls.Add(this.menuStrip1);
             this.Controls.Add(this.tabControl1);
